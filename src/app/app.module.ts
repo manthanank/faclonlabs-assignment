41296f2b2518111bb6465fb14bfd1933bae0d3cd
manthanank <manthan.ank46@gmail.com>
2021-12-05 20:37:48 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from './material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MaterialModule,
     AppRoutingModule
   ],
   providers: [],
