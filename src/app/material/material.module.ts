6e5fbcd1889fac7762e157f77645631d929875b6
manthanank <manthan.ank46@gmail.com>
2021-12-08 00:37:30 +0530

    commit

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,9 +3,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSortModule } from '@angular/material/sort';
 
 const MaterialComponents = [
-  MatToolbarModule, MatButtonModule, MatDialogModule, MatGridListModule
+  MatToolbarModule, MatButtonModule, MatDialogModule, MatGridListModule,MatSortModule
 ];
 
 @NgModule({
