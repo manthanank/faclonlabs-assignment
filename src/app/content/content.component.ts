f7881960c304ccb5a15a055be157d0040500fc3d
manthanank <manthan.ank46@gmail.com>
2021-12-07 15:39:59 +0530

    commit

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, Inject, Input } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms'
+import { FormBuilder, FormGroup } from '@angular/forms'
 import { DataService } from '../shared/data.service';
 import { ContentModel } from './content.model';
-import { Validators ,FormsModule,NgForm } from '@angular/forms';
+import { Validators, FormsModule, NgForm } from '@angular/forms';
 import { MatDialog } from "@angular/material/dialog";
+import { MatConfirmDialogComponent } from '../mat-confirm-dialog/mat-confirm-dialog.component';
 
 
 @Component({
@@ -15,62 +16,72 @@ import { MatDialog } from "@angular/material/dialog";
 
 export class ContentComponent implements OnInit {
   formValue !: FormGroup;
-  constructor(private formbuilder: FormBuilder,private data : DataService,private dialog: MatDialog) { }
+  constructor(private formbuilder: FormBuilder, private data: DataService, private dialog: MatDialog) { }
   contentModelObj: ContentModel = new ContentModel();
   formData !: any;
   //showAdd ! : boolean;
   //showUpdate ! : boolean;
-
-  ngOnInit():void {
+  dialogRef: any;
+  ngOnInit(): void {
     this.formValue = this.formbuilder.group({
+      id: [null, Validators.required],
       name: [null, Validators.required],
-      email: [null, Validators.compose([Validators.required,Validators.email])],
+      email: [null, Validators.compose([Validators.required, Validators.email])],
       address: [null, Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(500)])],
       number: [null, Validators.required]
     })
     this.getAllUser();
   }
   onSubmit() {
-  // TODO: Use EventEmitter with form value
-  console.warn(this.formValue.value);
-}
+    // TODO: Use EventEmitter with form value
+    console.warn(this.formValue.value);
+  }
   // clickAddInform(){
   //   this.formValue.reset();
   //   this.showAdd = true;
   //   this.showUpdate = false;
   // }
-  postUserDetails(){
+  postUserDetails() {
+    this.contentModelObj.id = this.formValue.value.id;
     this.contentModelObj.name = this.formValue.value.name;
     this.contentModelObj.email = this.formValue.value.email;
     this.contentModelObj.address = this.formValue.value.address;
     this.contentModelObj.number = this.formValue.value.number;
     this.data.postUser(this.contentModelObj)
-    .subscribe(res=>{
-      console.log(res);
-      alert("added")
-      let ref = document.getElementById('cancel')
-      ref ?.click();
-      this.formValue.reset();
-      this.getAllUser();
-    },
-    err=>{
-      alert('error')
-    })
+      .subscribe(res => {
+        console.log(res);
+        alert("added")
+        let ref = document.getElementById('cancel')
+        ref?.click();
+        this.formValue.reset();
+        this.getAllUser();
+      },
+        err => {
+          alert('error')
+        })
   }
-  getAllUser(){
+  getAllUser() {
     this.data.getUser()
-    .subscribe(res =>{
-      this.formData = res;
-    })
+      .subscribe(res => {
+        this.formData = res;
+      })
   }
-  deleteUserDetails(row:any){
-     this.data.deleteUser(row.id)
-     .subscribe(res =>{
-       alert('Deleted')
-       this.getAllUser();
-    })
+  deleteUserDetails(row: any) {
+    this.dialogRef = this.dialog.open(MatConfirmDialogComponent, {
+      //disableClose: false
+    });
+    this.dialogRef.afterClosed().subscribe((result: boolean) => {
+      if (result) {
+        this.data.deleteUser(row.id)
+          .subscribe(res => {
+            this.getAllUser();
+          })
+      }
+      this.dialogRef = null;
+    });
+
   }
-  onEdit(row:any){
+  onEdit(row: any) {
     //this.showAdd = false;
     //this.showUpdate = true;
     this.contentModelObj.id = row.id;
@@ -79,14 +90,14 @@ export class ContentComponent implements OnInit {
     this.formValue.controls['address'].setValue(row.address);
     this.formValue.controls['number'].setValue(row.number);
   }
-  updateUserDetails(){
+  updateUserDetails() {
+    this.contentModelObj.id = this.formValue.value.id;
     this.contentModelObj.name = this.formValue.value.name;
     this.contentModelObj.email = this.formValue.value.email;
     this.contentModelObj.address = this.formValue.value.address;
     this.contentModelObj.number = this.formValue.value.number;
-    this.data.updateUser(this.contentModelObj,this.contentModelObj.id)
-    .subscribe(res =>
-      {
+    this.data.updateUser(this.contentModelObj, this.contentModelObj.id)
+      .subscribe(res => {
         alert('updated')
         let ref = document.getElementById('cancel')
         ref?.click();
@@ -94,7 +105,8 @@ export class ContentComponent implements OnInit {
         this.getAllUser();
       })
   }
-  openDialog(){
+  openDialog() {
     this.dialog.open(ContentComponent)
+
   }
 }
